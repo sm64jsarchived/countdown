d1d0295144d504f51a52bf363c4a04dd0f51f7b2
GManTheMarioGuy <155935644+GManTheMarioGuy@users.noreply.github.com>
2025-08-23 20:34:45 -0500

    countdown

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+const PORT = 3000
+
+const endTime = new Date(Date.UTC(2025, 8, 6, 18, 0, 0)).getTime()
+
+app.use(express.static('public'))
+
+setInterval(() => {
+  const now = Date.now()
+  const remaining = endTime - now
+  io.emit('countdown', remaining > 0 ? remaining : 0)
+}, 1000)
+
+http.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`)
+})
