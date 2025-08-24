95f95b688e180683444d607fd956902dee9f7612
GManTheMarioGuy <155935644+GManTheMarioGuy@users.noreply.github.com>
2025-08-23 21:37:51 -0500

    Hopefully fix deployment issue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const endTime = new Date(Date.UTC(2025, 8, 6, 18, 0, 0)).getTime()
 
